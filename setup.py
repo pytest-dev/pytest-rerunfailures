41b25be57ffa317bea075cd60176b20902c68906
Florian Pilz <fp@gocept.com>
2016-02-05 10:48:35 +0100

    Do not add CHANGES to long description, since this breaks the tox runner.
    - tox only copies the README file, not the CHANGES file ...

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(name='pytest-rerunfailures',
       version='1.0.2.dev0',
       description='pytest plugin to re-run tests to eliminate flaky failures',
       long_description=(
-          open('README.rst').read() +
-          '\n\n' +
-          open('CHANGES.rst').read()),
+          open('README.rst').read()),
       author='Leah Klearman',
       author_email='lklrmn@gmail.com',
       url='https://github.com/gocept/pytest-rerunfailures',
