1ad001b33e8833678d4aa0d6797f6ca85512235b
Steffen Allner <sa@gocept.com>
2018-10-04 12:00:36 +0200

    Back to development: 4.3

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@ Changelog
 ---------
 
 
+4.3 (unreleased)
+================
+
+- Nothing changed yet.
+
+
 4.2 (2018-10-04)
 ================
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytest-rerunfailures',
-      version='4.2',
+      version='4.3.dev0',
       description='pytest plugin to re-run tests to eliminate flaky failures',
       long_description=(
           open('README.rst').read() +
