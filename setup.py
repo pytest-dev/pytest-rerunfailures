pytest-dev/pytest-rerunfailures

commit 43e8c38abe2efb52a03b93a5ea078946fca2a41f

Drop Python 2 support add support for Python 3.8.

setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme, open('CHANGES.rst') as changelog:
         changelog.read())
 
 setup(name='pytest-rerunfailures',
-      version='8.1.dev0',
+      version='9.0.dev0',
       description='pytest plugin to re-run tests to eliminate flaky failures',
       long_description=long_description,
       author='Leah Klearman',
@@ -16,7 +16,10 @@ setup(name='pytest-rerunfailures',
       url='https://github.com/pytest-dev/pytest-rerunfailures',
       py_modules=['pytest_rerunfailures'],
       entry_points={'pytest11': ['rerunfailures = pytest_rerunfailures']},
-      install_requires=['setuptools>=40.0', 'pytest >= 4.4'],
+      install_requires=[
+          'setuptools>=40.0',
+          'pytest >= 5.0',
+      ],
       license='Mozilla Public License 2.0 (MPL 2.0)',
       keywords='py.test pytest rerun failures flaky',
       zip_safe=False,
@@ -31,10 +34,10 @@ setup(name='pytest-rerunfailures',
           'Topic :: Software Development :: Quality Assurance',
           'Topic :: Software Development :: Testing',
           'Topic :: Utilities',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: Implementation :: CPython',
           'Programming Language :: Python :: Implementation :: PyPy',
       ])

.travis.yml
@@ -1,49 +1,16 @@
 language: python
-dist: xenial
-sudo: true
 python:
-  - 2.7
   - 3.5
   - 3.6
   - 3.7
-  - 3.7-dev
-  - pypy2.7-5.10.0
-  - pypy3.5
+  - 3.8
+  - pypy3
 env:
-  - PYTEST=4.4.2
-  - PYTEST=4.5.0
-  - PYTEST=4.6.6
-matrix:
-  include:
-    - python: 3.5
-      env: PYTEST=5.0.1
-    - python: 3.6
-      env: PYTEST=5.0.1
-    - python: 3.7
-      env: PYTEST=5.0.1
-    - python: pypy3.5
-      env: PYTEST=5.0.1
-    - python: 3.5
-      env: PYTEST=5.1.3
-    - python: 3.6
-      env: PYTEST=5.1.3
-    - python: 3.7
-      env: PYTEST=5.1.3
-    - python: pypy3.5
-      env: PYTEST=5.1.3
-    - python: 3.5
-      env: PYTEST=5.2.1
-    - python: 3.6
-      env: PYTEST=5.2.1
-    - python: 3.7
-      env: PYTEST=5.2.1
-
-    - python: 3.7
-      env: PYTEST=5.3.5
-    - python: 3.7
-      env: PYTEST=5.4.0
-    - python: pypy3.5
-      env: PYTEST=5.2.1
+  - PYTEST=5.0.1
+  - PYTEST=5.1.3
+  - PYTEST=5.2.4
+  - PYTEST=5.3.5
+  - PYTEST=5.4.1
 install:
   - pip install -q pytest==$PYTEST
   - pip install -q -e .

setup.cfg
@@ -1,5 +1,2 @@
 [bdist_wheel]
-universal = 1
-
-[zest.releaser]
-create-wheel = yes
+universal = 0

test_pytest_rerunfailures.py
@@ -1,14 +1,7 @@
+import mock
+import pytest
 import random
 import time
-
-try:
-    from unittest import mock
-except ImportError:
-    import mock
-
-import pytest
-import pkg_resources
-
 
 
 pytest_plugins = 'pytester'

tox.ini
@@ -4,15 +4,12 @@
 # and then run "tox" from this directory.
 
 [tox]
-envlist = py{27,35,36,37,py,py3}-pytest{44,45,46},py{35,36,37,py3}-pytest{50,51,52,53,54},
+envlist = py{35,36,37,38,py3}-pytest{50,51,52,53,54},
 
 [testenv]
 commands = py.test test_pytest_rerunfailures.py {posargs}
 deps =
     mock
-    pytest44: pytest==4.4.*
-    pytest45: pytest==4.5.*
-    pytest46: pytest==4.6.*
     pytest50: pytest==5.0.*
     pytest51: pytest==5.1.*
     pytest52: pytest==5.2.*

commit 992a9290a990be2cd95c097091010370d0bed053

Reduce pytest requirement to 4.4
The code and tests still work with pytest 4.4

Fixes: commit 43e8c38abe2efb52a03b93a5ea078946fca2a41f

setup.py
@@ -13,7 +13,7 @@ setup(
     url="https://github.com/pytest-dev/pytest-rerunfailures",
     py_modules=["pytest_rerunfailures"],
     entry_points={"pytest11": ["rerunfailures = pytest_rerunfailures"]},
-    install_requires=["setuptools>=40.0", "pytest >= 5.0"],
+    install_requires=["setuptools>=40.0", "pytest >= 4.4"],
     python_requires=">=3.5",
     license="Mozilla Public License 2.0 (MPL 2.0)",
     keywords="py.test pytest rerun failures flaky",