242e1d4ceacff4c809189222d8560a094524c322
Michael Howitz <mh@gocept.com>
2021-05-26 15:14:33 +0200

    Fix future version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as readme, open("CHANGES.rst") as changelog:
 
 setup(
     name="pytest-rerunfailures",
-    version="9.2.dev0",
+    version="10.0.dev0",
     description="pytest plugin to re-run tests to eliminate flaky failures",
     long_description=long_description,
     author="Leah Klearman",
