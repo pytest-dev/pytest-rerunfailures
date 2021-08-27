17b59511b06f8ba8fc827da323b3afb571fedeb8
Paul Wise <pabs3@bonedaddy.net>
2021-08-27 19:58:28 +0800

    Clean up packaging and build system (#167)
    * Tell git to not ignore egg files that are not the correct .egg-info/ dir
    * Tell git to ignore the dist/ directory where sdist/bdist files are stored
    * Make the setup.py executable without prefixing it with python
    * Migrate packaging metadata from setup.py to declarative setup.cfg
    * Add PEP-518 pyproject.toml file

--- setup.py
+++ setup.py
@@ -1,44 +1,4 @@
+#!/usr/bin/env python
 from setuptools import setup
 
-with open("README.rst") as readme, open("CHANGES.rst") as changelog:
-    long_description = ".. contents::\n\n" + readme.read() + "\n\n" + changelog.read()
-
-setup(
-    name="pytest-rerunfailures",
-    version="10.2.dev0",
-    description="pytest plugin to re-run tests to eliminate flaky failures",
-    long_description=long_description,
-    author="Leah Klearman",
-    author_email="lklrmn@gmail.com",
-    url="https://github.com/pytest-dev/pytest-rerunfailures",
-    py_modules=["pytest_rerunfailures"],
-    entry_points={"pytest11": ["rerunfailures = pytest_rerunfailures"]},
-    install_requires=[
-        "setuptools>=40.0",
-        "pytest >= 5.3",
-    ],
-    python_requires=">=3.6",
-    license="Mozilla Public License 2.0 (MPL 2.0)",
-    keywords="py.test pytest rerun failures flaky",
-    zip_safe=False,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Pytest",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS :: MacOS X",
-        "Topic :: Software Development :: Quality Assurance",
-        "Topic :: Software Development :: Testing",
-        "Topic :: Utilities",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
-)
+setup()
